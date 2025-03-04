a51658cda90f7fbb44f643c419c0d715f1b1336a
DevKike <alvaronarvaez151@gmail.com>
2025-03-03 23:08:19 -0500

    Feat: add class-validator middleware and DTO for account creation

--- src/infrastructure/adapters/http/driving/account/account-router.adapter.ts
+++ src/infrastructure/adapters/http/driving/account/account-router.adapter.ts
@@ -1,22 +1,50 @@
-import { Router } from 'express';
-import { IRouterModulePort } from '../../../../../domain/ports/http/router-module.port.interface';
+import { Response, Router } from 'express';
+import { IRouterModulePort } from '../../../../../domain/ports/http/routers/router-module.port.interface';
+import { inject, injectable } from 'inversify';
+import { ICreateAccountUseCase } from '../../../../../domain/use-cases/account/create-account.use-case.interface';
+import { TYPES } from '../../../../inversify/types/inversify.types';
+import { IResponseManagerPort } from '../../../../../domain/ports/http/response/response-manager.port.interface';
 import { HttpStatusCode } from '../../../../../domain/enums/http/http-status-codes.enum';
-import { injectable } from 'inversify';
+import { SUCCESS_MESSAGES } from '../../../../../domain/constants/success-messages.constant';
+import { CustomError } from '../../../../../domain/exceptions/custom-error.exception';
+import { AccountCreateReqDto } from '../../../../class-validator/dtos/req/account-create.req,dto';
+import { ClassValidatorMiddlewareFactory } from '../../middlewares/class-validator.middleware';
+import { container } from '../../../../inversify/config/inversify.config';
 
 @injectable()
 export class AccountRouterAdapter implements IRouterModulePort<Router> {
   private readonly _router: Router;
-  constructor() {
+  private readonly _classValidator: ReturnType<
+    typeof ClassValidatorMiddlewareFactory.create
+  >;
+
+  constructor(
+    @inject(TYPES.ResponseManagerAdapter)
+    private readonly _responseManagerAdapter: IResponseManagerPort<
+      Response,
+      CustomError
+    >,
+    @inject(TYPES.CreateAccountUseCase)
+    private readonly _createAccountUseCase: ICreateAccountUseCase
+  ) {
     this._router = Router();
+    this._classValidator = ClassValidatorMiddlewareFactory.create(container);
     this.initRoutes();
   }
 
   initRoutes(): void {
-    this._router.post('/account', (req, res) => {
-      res
-        .status(HttpStatusCode.CREATED)
-        .send({ message: 'Account created with success!' });
-    });
+    this._router.post(
+      '/account',
+      this._classValidator(AccountCreateReqDto),
+      (req, res) => {
+        this._responseManagerAdapter.manageSuccess(
+          this._createAccountUseCase.execute(req.body),
+          res,
+          HttpStatusCode.CREATED,
+          SUCCESS_MESSAGES.CREATED
+        );
+      }
+    );
   }
 
   getRouter(): Router {
---
+++ src/infrastructure/class-validator/dtos/req/account-create.req,dto.ts
@@ -0,0 +1,24 @@
+import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
+import {
+  AccountStatus,
+  AccountType,
+} from '../../../../domain/enums/account/account.enum';
+import { IAccountCreate } from '../../../../domain/models/account/account-model.interface';
+
+export class AccountCreateReqDto implements IAccountCreate {
+  @IsNotEmpty()
+  @IsNumber()
+  number: number;
+
+  @IsNotEmpty()
+  @IsEnum(AccountType)
+  type: AccountType;
+
+  @IsNotEmpty()
+  @IsNumber()
+  balance: number;
+
+  @IsNotEmpty()
+  @IsEnum(AccountStatus)
+  status: AccountStatus;
+}
